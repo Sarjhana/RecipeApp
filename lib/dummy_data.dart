26bcc4fc457eeb12204befe099d74a40484666ff
unknown <sarjhur@gmail.com>
2020-03-30 23:22:41 +0530

    mainscreen styled

---
+++ lib/dummy_data.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    title: 'German',
+    color: Colors.blueGrey,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Light & Lovely',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Exotic',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Breakfast',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Asian',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c9',
+    title: 'French',
+    color: Colors.teal,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Summer',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c11',
+    title: 'North Indian',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c12',
+    title: 'South Indian',
+    color: Colors.deepPurple,
+  ),
+  Category(
+    id: 'c13',
+    title: 'Mexican',
+    color: Colors.indigo,
+  ),
+  Category(
+    id: 'c14',
+    title: 'Arabian',
+    color: Colors.cyanAccent,
+  ),
+];
