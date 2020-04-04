98bb3e3753652f3d5e0f4f3da8f83057a60c782d
unknown <sarjhur@gmail.com>
2020-04-05 01:45:08 +0530

    added favourites page

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -39,12 +39,6 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
     }
     super.didChangeDependencies();
   }
-
-  void _removeMeal(String mealId){
-    setState(() {
-      displayedMeals.removeWhere((meal) => meal.id == mealId);
-    });
-  }
   @override
   Widget build(BuildContext context) {
 
@@ -58,7 +52,6 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
           duration: displayedMeals[index].duration,
           complexity: displayedMeals[index].complexity,
           affordability: displayedMeals[index].affordability,
-          removeItem: _removeMeal,
         );
       },
       itemCount: displayedMeals.length,
